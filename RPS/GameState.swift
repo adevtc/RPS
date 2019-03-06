bf9286f8447c42aebd67967215c9ff9d29e81c0a
Jon <jontierney@m4cb00k.local>
2019-03-05 19:00:05 -0500

    initial commit

---
+++ RPS/GameState.swift
@@ -0,0 +1,14 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by Jon on 1/5/19.
+//  Copyright © 2019 addevtec. All rights reserved.
+//
+
+import Foundation
+import GameplayKit
+
+enum GameState {
+    case start, win, lose, draw
+}
--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -10,11 +10,101 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var robotLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var rockButton: UIButton!
+    @IBOutlet weak var paperButton: UIButton!
+    @IBOutlet weak var scissorsButton: UIButton!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var botChoiceLabel: UILabel!
+    @IBOutlet weak var yourChoiceLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        playAgainButton.isHidden = true
+        botChoiceLabel.isHidden = true
+        yourChoiceLabel.isHidden = true
+    
     }
 
-
+    func resetBoard() {
+        robotLabel.text = "🤖"
+        titleLabel.text = "Rock, Paper, Scissors?"
+        rockButton.isHidden = false
+        rockButton.isEnabled = true
+        paperButton.isHidden = false
+        paperButton.isEnabled = true
+        scissorsButton.isHidden = false
+        scissorsButton.isEnabled = true
+        playAgainButton.isHidden = true
+        botChoiceLabel.isHidden = true
+        yourChoiceLabel.isHidden = true
+    }
+    
+    func play(_ playerTurn: Sign) {
+        rockButton.isEnabled = false
+        paperButton.isEnabled = false
+        scissorsButton.isEnabled = false
+        
+        let opponent = randomSign()
+        
+        let gameResult = playerTurn.takeTurn(opponent)
+        robotLabel.text = opponent.emoji
+        
+        switch gameResult {
+        case .draw:
+            botChoiceLabel.isHidden = false
+            yourChoiceLabel.isHidden = false
+            titleLabel.text = "It's a draw."
+        case .lose:
+            botChoiceLabel.isHidden = false
+            yourChoiceLabel.isHidden = false
+            titleLabel.text = "Sorry, you lost... RIP ☹️ "
+        case .win:
+            botChoiceLabel.isHidden = false
+            yourChoiceLabel.isHidden = false
+            titleLabel.text = "You won! 😃"
+        case .start:
+            titleLabel.text = "Rock, Paper, Scissors?"
+        }
+        
+        switch playerTurn {
+        case .rock:
+            rockButton.isHidden = false
+            paperButton.isHidden = true
+            scissorsButton.isHidden = true
+        case .paper:
+            rockButton.isHidden = true
+            paperButton.isHidden = false
+            scissorsButton.isHidden = true
+        case .scissors:
+            rockButton.isHidden = true
+            paperButton.isHidden = true
+            scissorsButton.isHidden = false
+        }
+        
+        playAgainButton.isHidden = false
+        
+    }
+    
+    
+    @IBAction func playAgainSelected(_ sender: Any) {
+        resetBoard()
+    }
+    
+    @IBAction func rockSelected(_ sender: Any) {
+        play(Sign.rock)
+    }
+    
+    @IBAction func paperSelected(_ sender: Any) {
+        play(Sign.paper)
+    }
+    
+    @IBAction func scissorsSelected(_ sender: Any) {
+    play(Sign.scissors)
+    }
+    
 }
 
